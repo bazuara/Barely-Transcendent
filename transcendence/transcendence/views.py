95a0656f17b6be1191b2d1b5a989f4f56964f318
Bruno Azuara <bruno@azuara.dev>
2025-03-07 18:33:09 +0000

    add anonimization functionality on profile

--- transcendence/transcendence/views.py
+++ transcendence/transcendence/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from users.models import User
 from django.utils import timezone
 
+
 def home(request):
     # Obtener el ID del usuario de la sesión
     user_id = request.session.get('user_id')
-    
+
     if not user_id:
         return redirect('login')
-    
+
     # Obtener el usuario de la base de datos
     try:
         user = User.objects.get(internal_id=user_id)
@@ -19,10 +20,11 @@ def home(request):
         # Si el usuario no existe, redirigir a login
         request.session.flush()
         return redirect('login')
-    
+
     template = "partials/home.html" if request.htmx else "home.html"
-    
+
     return render(request, template)
+
 
 def about(request):
     template = "partials/about.html" if request.htmx else "about.html"
