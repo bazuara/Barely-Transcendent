97b863b6f66e46de788045e81a5328bf132318fc
c3nz <agapitobusiness@gmail.com>
2025-03-21 11:37:25 +0000

    mock-login updated

--- transcendence/transcendence/urls.py
+++ transcendence/transcendence/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('profile/update/', update_profile, name='update_profile'),  # Añadir ruta para actualizar perfil
     path('pong/', views.pong, name='pong'),
     path('tournament/', views.tournament, name='tournament'),
-    path('mock-login/', mock_login, name='mock_login'),
+    path('mock-login/<str:username>/', mock_login, name='mock_login'),
     path('profile/anonimize/', anonimize, name='anonimize'),
     path("prometheus/", include("django_prometheus.urls")),
 ]
